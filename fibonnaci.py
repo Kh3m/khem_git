7c46661d4125b72870a3350e8a198860c4602f5e
Kh3m <khem6333@gmail.com>
2018-09-25 09:24:23 +0100

    Add fibonacci.py

---
+++ fibonnaci.py
@@ -0,0 +1,14 @@
+
+class Fibonacci: 
+    
+    def __init__ (self, a, b) :
+        self.a, self.b = a,b
+
+    def print_list_of_fib(self) :
+        print(self.a)
+        print(self.b)
+
+fib = Fibonacci(90, 120)
+
+fib.print_list_of_fib()
+
